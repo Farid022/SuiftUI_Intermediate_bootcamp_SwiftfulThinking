1a58e10530c50e2c756e0fcae28be71e673ec34a
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2021-12-19 04:58:26 +0500

    Initial Commit

---
+++ SuiftUI_Intermediate_bootcamp/SuiftUI_Intermediate_bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuiftUI_Intermediate_bootcampApp.swift
+//  SuiftUI_Intermediate_bootcamp
+//
+//  Created by Muhammad Farid Ullah on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SuiftUI_Intermediate_bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
